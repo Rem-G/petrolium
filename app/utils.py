d13f6154fa162097b3900c7451a8dcee510034ee
Rem-G <gosselin.rem@gmail.com>
2020-08-17 19:33:16 +0200

    Map update

--- app/utils.py
+++ app/utils.py
@@ -5,6 +5,8 @@ import zipfile
 import xmltodict
 import json
 import time
+from pyproj import Proj, transform
+
 
 from django.contrib.gis.geos import Point, GeometryCollection
 from django.conf import settings
@@ -14,6 +16,8 @@ class Petrol:
 	def __init__(self):
 		self.data_download_url = "https://donnees.roulez-eco.fr/opendata/instantane"
 		self.static_path = settings.MEDIA_ROOT + '/data/'
+		self.create_petrol_data()
+		self.geojson_data = self.xml_to_json(self.static_path, 'PrixCarburants_instantane')
 
 	def download_petrol_data(self, requests_url, static_url, filename, chunk_size=128):
 		r = requests.get(requests_url, stream=True)
@@ -29,53 +33,71 @@ class Petrol:
 		with open(static_url + xml_name + '.xml', 'rb') as fd:
 			doc = xmltodict.parse(fd.read())
 
-		with open(self.static_path+'data.json', 'w', encoding='utf-8') as f:
-			#json.dump(doc, f, ensure_ascii=False, indent=4)
-			geo_json = {
-							"type": "FeatureCollection",
-							"features": []
-						}
+		geo_json = {
+						"type": "FeatureCollection",
+						"features": []
+					}
 
-
-			for pdv in doc.get('pdv_liste').get('pdv'):
-
-				geo_json['features'].append(
-							{
-								"type": "Feature",
-								"properties": {
-									"id":  pdv.get('@id'),
-									"cp": pdv.get('@cp'),
-									"pop": pdv.get('@pop'),
-									"adresse": pdv.get('adresse'),
-									"ville": pdv.get('ville'),
-									"services": pdv.get('services'),
-									"prix": pdv.get('prix')
-									},
-								"geometry": {
-									"type": "Point",
-									"coordinates": [float(pdv.get('@longitude'))/100000,float(pdv.get('@latitude'))/100000]
-								}
+		for pdv in doc.get('pdv_liste').get('pdv'):
+			geo_json['features'].append(
+						{
+							"type": "Feature",
+							"properties": {
+								"id":  pdv.get('@id'),
+								"cp": pdv.get('@cp'),
+								"pop": pdv.get('@pop'),
+								"adresse": pdv.get('adresse'),
+								"ville": pdv.get('ville'),
+								"services": pdv.get('services'),
+								"prix": pdv.get('prix')
+								},
+							"geometry": {
+								"type": "Point",
+								"coordinates": [float(pdv.get('@longitude'))/100000,float(pdv.get('@latitude'))/100000]
 							}
-				)
-			
-			json.dump(geo_json, f, ensure_ascii=False, indent=4)
+						}
+			)
+		
+		return geo_json
 
 	def create_petrol_data(self):
 		now = time.time()
-		if (not os.path.isfile(self.static_path + 'data.json')
-			or os.path.getctime(self.static_path + 'data.json') < 24*3600):
+		if (not os.path.isfile(self.static_path + 'PrixCarburants_instantane.xml')
+			or os.path.getctime(self.static_path + 'PrixCarburants_instantane.xml') < 24*3600):
 
 			self.download_petrol_data(self.data_download_url, self.static_path, 'data.zip')
 			self.extract_zip(self.static_path, 'data.zip')
-			self.xml_to_json(self.static_path, 'PrixCarburants_instantane')
 
 			os.remove(self.static_path + 'data.zip')
-			os.remove(self.static_path + 'PrixCarburants_instantane.xml')
 
-	def get_petrol_data(self):
-		self.create_petrol_data()
-		json_file = open(self.static_path + 'data.json')
-		return json.load(json_file)
+	def get_petrol_data(self, bbox):
+		bbox_list = [float(b) for b in bbox.split(',')]
+
+		inProj = Proj('EPSG:3857')
+		outProj = Proj('EPSG:4326')
+		y1, x1 = transform(inProj,outProj, bbox_list[0], bbox_list[1])
+		y2, x2 = transform(inProj,outProj, bbox_list[2], bbox_list[3])
+
+		geo_json = {
+						"type": "FeatureCollection",
+						"features": []
+					}
+
+		for feature in self.geojson_data.get('features'):
+			coor = feature['geometry']['coordinates']
+			if (coor[0] > x1
+				and coor[0] <= x2
+				and coor[1] >= y1
+				and coor[1] <= y2):
+
+				geo_json['features'].append(feature)
+
+		print(len(geo_json['features']))
+
+		return geo_json
+
+
+
 
 
 
--- app/views.py
+++ app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
-
 from .utils import Petrol
+p = Petrol()
+
 
 def home(request):
     return render(request, 'map.html', locals())
 
 def map(request):
-    p = Petrol()
-    return JsonResponse(p.get_petrol_data())
+    bbox = request.GET.get('in_bbox')
+    return JsonResponse(p.get_petrol_data(bbox))
